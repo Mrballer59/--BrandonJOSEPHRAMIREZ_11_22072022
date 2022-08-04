afd4568dc0a52be21b51028ea8956214cddf176a
B <77552838+Mrballer59@users.noreply.github.com>
2022-08-04 22:01:24 +0200

    showing content on props page + css

--- src/components/DropdownList/DropdownList.jsx
+++ src/components/DropdownList/DropdownList.jsx
@@ -2,17 +2,36 @@ import React from "react";
 import "./DropdownList.css";
 import arrDown from "../../assets/arrow_down.png";
 import arrUp from "../../assets/arrow_up.png";
+import { useState } from "react";
 
-const DropdownList = ({ title, text }) => {
-  return (
-    <div className="dropdownList">
+function DropdownList({ title, text, extra }) {
+  const [showDropList, setDropList] = useState(false);
+
+  return showDropList ? (
+    <div className={extra ? "droplist" + extra : "droplist"}>
       <div className="dp_Header">
         <h3>{title}</h3>
-        <img src={arrDown} alt="Display content" />
+        <img
+          src={arrUp}
+          alt="Display content"
+          onClick={() => setDropList(false)}
+        />
       </div>
+
       <p>{text}</p>
     </div>
+  ) : (
+    <div className="droplist">
+      <div className="dp_Header">
+        <h3>{title}</h3>
+        <img
+          src={arrDown}
+          alt="Display content"
+          onClick={() => setDropList(true)}
+        />
+      </div>
+    </div>
   );
-};
+}
 
 export default DropdownList;
