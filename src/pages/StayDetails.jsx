f8b5e0af96486f38c7e47284e2311db8e03e693b
B <77552838+Mrballer59@users.noreply.github.com>
2022-08-09 14:23:51 +0200

    added tags plus dropdown

--- src/pages/StayDetails.jsx
+++ src/pages/StayDetails.jsx
@@ -3,11 +3,13 @@ import "../style/StayDetails.css";
 import { Link, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Tag from "../components/Tags/Tag";
+import DropDown from "../components/DropdownList/DropdownList";
 
 const StayDetails = () => {
   const [product, setProduct] = useState(null);
   const { stayDetailsId } = useParams();
-
+  // need to fetch the data then need to check if it's correctly grabbed then you need to store it and compared to the id
   useEffect(() => {
     axios
       .get("../DataLogement.json")
@@ -24,8 +26,39 @@ const StayDetails = () => {
 
   return (
     <div className="listing">
-      <h4>{product && product.title}</h4>
-      <h6>{product && product.location}</h6>
+      <section className="listingSection">
+        <div className="carousel"></div>
+        <div className="accomodationDetails">
+          <div>
+            <h4>{product && product.title}</h4>
+            <h6>{product && product.location}</h6>
+            <div className="tagsSection">
+              {product && product.tags.map((tag) => <Tag tagName={tag} />)}
+            </div>
+          </div>
+          <div className="hostListing">
+            <h5>{product && product.host.name}</h5>
+            <img
+              src={product && product.host.picture}
+              alt={product && product.host.name}
+            />
+          </div>
+        </div>
+        <div className="listingDropDown">
+          <DropDown title="Description" text={product && product.description} />
+          <DropDown
+            title="Équipements"
+            text={
+              product &&
+              product.equipments.map((equipment) => (
+                <ul>
+                  <li>{equipment}</li>
+                </ul>
+              ))
+            }
+          />
+        </div>
+      </section>
     </div>
   );
 };
